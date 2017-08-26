21e126bf963bd4f829332372da26a4fdb515b60c
Frank Vinci <fv42wid@yahoo.com>
2017-08-26 15:42:47 -0400

    displaying assignments on employee, add new to display next

--- app/javascript/packs/employees/vue-employee.vue
+++ app/javascript/packs/employees/vue-employee.vue
@@ -10,10 +10,29 @@
                 </v-card-title>
                 <v-card-actions>
                     <v-btn flat class="orange--text" @click="createAssignment()">Create Assignment</v-btn>
+                    <v-btn flat class="orange--text" @click="showAssignments = !showAssignments">Show Assignments</v-btn>
                 </v-card-actions>
                 <v-card-text v-if="creatingAssignment">
                     <h6>Create Assignment</h6>
                     <new-assignment :employee="employee.id"></new-assignment>
+                </v-card-text>
+                <v-card-text v-if="showAssignments">
+                    <v-list two-line>
+                        <v-list-tile avatar ripple v-for="(assignment, index) in assignments" :key="index">
+                            <v-list-tile-content>
+                                <v-list-tile-title class="primary--text">{{ assignment.project }}</v-list-tile-title>
+                                <v-list-tile-sub-title class="grey--text">
+                                    {{ assignment.start_date | formatDate }} to {{ assignment.end_date| formatDate }}
+                                </v-list-tile-sub-title>
+                                <v-list-tile-sub-title>
+                                    {{ assignment.allocation_percentage }}% - {{ assignment.project_frequency }}
+                                </v-list-tile-sub-title>
+                            </v-list-tile-content>
+                            <v-list-tile-action>
+                                <v-icon class="grey--text text--lighten-1">create</v-icon>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list>
                 </v-card-text>
             </v-card>
         </v-flex>
@@ -28,7 +47,8 @@
             return {
                 employee: JSON.parse(this.card),
                 assignments: JSON.parse(this.empassignments),
-                creatingAssignment: false
+                creatingAssignment: false,
+                showAssignments: false
             }
         },
         props: ['card', 'empassignments'],
@@ -39,11 +59,16 @@
         methods: {
             createAssignment() {
                 console.log('create assignment')
-                this.creatingAssignment = true
+                this.creatingAssignment = !this.creatingAssignment
             }
         },
         components: {
             'new-assignment' : AssignmentNew
+        },
+        filters: {
+            formatDate: function(date) {
+                return date.slice(0, 10)
+            }
         }
     }
 </script>
